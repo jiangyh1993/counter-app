1834d875d21846eef57902c6456d7ef98748b566
Unknown <158840284@qq.com>
2018-10-03 20:46:09 -0500

    Event handler
    add delete function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   // constructor() {
@@ -25,7 +25,7 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    this.setState({ value: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -38,21 +38,25 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()} */}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
